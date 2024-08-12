eccd7cd695e15765565e6ea8d0e6927cfe4b911c
Jorjeser-67 <108180103+Jorjeser-67@users.noreply.github.com>
2024-08-12 18:30:48 +0530

    Update config.py
    Signed-off-by: Jorjeser-67 <108180103+Jorjeser-67@users.noreply.github.com>

--- config.py
+++ config.py
@@ -5,23 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = 26241401
+API_HASH = bf695a75cecd4746336e77abb146b441
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = 7458379569:AAESyMMVDu3VdKcYnXVJtwUyqAUvgLcgWWY
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = t.me/novelemyen
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = BAGQaXkApFKXRGOIc8kX-ibdiSu3jgDzKB8R6XFwrdE-JriCHJ2W_ZPlnbOsCZXXMM6LfVozVGbTyrmxjbplCD0fZhKiVYxjxBJR3Rs5tovyraN03K9dYzd5rXeDdSgYPNQM8eoCnSpR5C5E_hOAy6gk20MduJKkXz84zlcusHIimyPPSGJpSxSW4C9Yay_SiFiBJXJC0wgDu7atHuqGp6l3vrwmzTBZY5CDcrRjmrTO4c-Bmd4rIdbastgI7q7vmR7kA0hPDy68pTKeNboyT-fO6uIkGV5l_FfLb0jgCfjzfbZeNHBSES2JC3Ap6fpqq2sBGifR14sH84g0Dk4IjHe0_iiyswAAAAGSKDX2AA
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
-
-
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
